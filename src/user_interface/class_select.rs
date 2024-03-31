f927d4a48f2d8194e89b9512712756a78b001a04
FranzLimberger <limofx@gmx.de>
2024-03-31 17:59:40 +0200

    insert class selection

--- src/user_interface/class_select.rs
+++ src/user_interface/class_select.rs
@@ -1,173 +1,224 @@
-use bevy::prelude::*;
 use crate::classes::class::*;
+use bevy::prelude::*;
+use std::str::FromStr;
 
-//TODO: look how we handle player character 
+//TODO: look how we handle player character
 #[derive(Component)]
 struct PlayerCharacter;
 
-//TODO: Create multiple buttons for class selections
-pub fn initialize_class_select_buttons(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>
-) {
-    commands
-    .spawn(NodeBundle {
-        style: Style {
-            width: Val::Percent(100.0),
-            height: Val::Percent(100.0),
-            align_items: AlignItems::Start,
-            justify_content: JustifyContent::Start,
-            flex_direction: FlexDirection::Column,
-            ..default()
-        },
-        ..default()
-    })
-    .with_children(|parent| {
-        parent.spawn(TextBundle::from_section(
-            "Choose your character",
-            TextStyle {
-                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                font_size: 40.0,
-                color: Color::rgb(0.9, 0.9, 0.9),
-            },
-        ));
-    })
-    .with_children(|parent| {
-        parent
-            .spawn(ButtonBundle {
-                style: Style {
-                    width: Val::Px(150.0),
-                    height: Val::Px(65.0),
-                    border: UiRect::all(Val::Px(5.0)),
-                    // horizontally center child text
-                    justify_content: JustifyContent::Center,
-                    // vertically center child text
-                    align_items: AlignItems::Center,
-                    ..default()
-                },
-                border_color: BorderColor(Color::BLACK),
-                ..default()
-            })
-            .with_children(|parent| {
-                parent.spawn(TextBundle::from_section(
-                    "Mage",
-                    TextStyle {
-                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                        font_size: 40.0,
-                        color: Color::rgb(0.9, 0.9, 0.9),
-                    },
-                ));
-            });
-    })
-    .with_children(|parent| {
-        parent
-            .spawn(ButtonBundle {
-                style: Style {
-                    width: Val::Px(150.0),
-                    height: Val::Px(65.0),
-                    border: UiRect::all(Val::Px(5.0)),
-                    // horizontally center child text
-                    justify_content: JustifyContent::Center,
-                    // vertically center child text
-                    align_items: AlignItems::Center,
-                    ..default()
-                },
-                border_color: BorderColor(Color::BLACK),
-                ..default()
-            })
-            .with_children(|parent| {
-                parent.spawn(TextBundle::from_section(
-                    "Hunter",
-                    TextStyle {
-                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                        font_size: 40.0,
-                        color: Color::rgb(0.9, 0.9, 0.9),
-                    },
-                ));
-            });
-    })
-    .with_children(|parent| {
-        parent
-            .spawn(ButtonBundle {
-                style: Style {
-                    width: Val::Px(150.0),
-                    height: Val::Px(65.0),
-                    border: UiRect::all(Val::Px(5.0)),
-                    // horizontally center child text
-                    justify_content: JustifyContent::Center,
-                    // vertically center child text
-                    align_items: AlignItems::Center,
-                    ..default()
-                },
-                border_color: BorderColor(Color::BLACK),
-                ..default()
-            })
-            .with_children(|parent| {
-                parent.spawn(TextBundle::from_section(
-                    "Priest",
-                    TextStyle {
-                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                        font_size: 40.0,
-                        color: Color::rgb(0.9, 0.9, 0.9),
-                    },
-                ));
-            });
-    })
-    .with_children(|parent| {
-        parent
-            .spawn(ButtonBundle {
-                style: Style {
-                    width: Val::Px(150.0),
-                    height: Val::Px(65.0),
-                    border: UiRect::all(Val::Px(5.0)),
-                    // horizontally center child text
-                    justify_content: JustifyContent::Center,
-                    // vertically center child text
-                    align_items: AlignItems::Center,
-                    ..default()
-                },
-                border_color: BorderColor(Color::BLACK),
-                ..default()
-            })
-            .with_children(|parent| {
-                parent.spawn(TextBundle::from_section(
-                    "Rogue",
-                    TextStyle {
-                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                        font_size: 40.0,
-                        color: Color::rgb(0.9, 0.9, 0.9),
-                    },
-                ));
-            });
-    })
-    .with_children(|parent| {
-        parent
-            .spawn(ButtonBundle {
-                style: Style {
-                    width: Val::Px(150.0),
-                    height: Val::Px(65.0),
-                    border: UiRect::all(Val::Px(5.0)),
-                    // horizontally center child text
-                    justify_content: JustifyContent::Center,
-                    // vertically center child text
-                    align_items: AlignItems::Center,
-                    ..default()
-                },
-                border_color: BorderColor(Color::BLACK),
-                ..default()
-            })
-            .with_children(|parent| {
-                parent.spawn(TextBundle::from_section(
-                    "Warrior",
-                    TextStyle {
-                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                        font_size: 40.0,
-                        color: Color::rgb(0.9, 0.9, 0.9),
-                    },
-                ));
-            });
-    });
+#[derive(Component)]
+pub struct ClassSelectButton;
 
+//pub static mut class_selected: bool = false;
+
+//mut query: Query<&mut Transform, With<MyCamera>>
+
+pub fn handle_character_selection(
+    mut interaction_query: Query<
+        (&Interaction, &Children),
+        (Changed<Interaction>, With<ClassSelectButton>),
+    >,
+    mut text_query: Query<&mut Text>,
+) {
+
+    /* unsafe {
+        if class_selected {
+            return;
+        }
+    } */
+
+    let mut selected_character: Character = Character::new(CharacterType::NonCharacter);
+
+    for (interaction, child) in &mut interaction_query {
+        let mut text = text_query.get_mut(child[0]).unwrap();
+        match *interaction {
+            Interaction::Pressed => {
+                match Character::from_str(&text.sections[0].value) {
+                    Ok(created_character) => {
+                        println!("you selected {}", text.sections[0].value);
+                        selected_character = created_character
+                    }
+                    Err(_e) => println!("no character selected"), //TODO: Proper error handling
+                }
+            }
+            Interaction::Hovered => {
+                
+            }
+            Interaction::None => {
+                
+            }
+        }
+    }
+    //TODO: change the unsafe and remove static to know if a class was selected
+    /* unsafe {
+        class_selected = true;
+    } */
+}
+
+//TODO: Create multiple buttons for class selections
+pub fn initialize_class_select_buttons(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Start,
+                justify_content: JustifyContent::Start,
+                flex_direction: FlexDirection::Column,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn(TextBundle::from_section(
+                "Choose your character",
+                TextStyle {
+                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                    font_size: 40.0,
+                    color: Color::rgb(0.9, 0.9, 0.9),
+                },
+            ));
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ClassSelectButton)
+                .insert(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    border_color: BorderColor(Color::BLACK),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Mage",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ClassSelectButton)
+                .insert(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    border_color: BorderColor(Color::BLACK),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Hunter",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ClassSelectButton)
+                .insert(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    border_color: BorderColor(Color::BLACK),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Priest",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ClassSelectButton)
+                .insert(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    border_color: BorderColor(Color::BLACK),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Rogue",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ClassSelectButton)
+                .insert(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    border_color: BorderColor(Color::BLACK),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Warrior",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
+        });
 }
 
 pub fn initialize_player(
@@ -175,10 +226,13 @@ pub fn initialize_player(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn(PlayerCharacter).insert(PbrBundle {
-        mesh: meshes.add(Cuboid::new(2f32, 2f32,2f32)),
-        material: materials.add(Color::rgb(1f32, 1f32, 5f32)),
-        transform: Transform::from_xyz(0f32, 1f32, 5f32),
-        ..default()
-    },).insert(Character::new(CharacterType::Warrior));
+    commands
+        .spawn(PlayerCharacter)
+        .insert(PbrBundle {
+            mesh: meshes.add(Cuboid::new(2f32, 2f32, 2f32)),
+            material: materials.add(Color::rgb(1f32, 1f32, 5f32)),
+            transform: Transform::from_xyz(0f32, 1f32, 5f32),
+            ..default()
+        })
+        .insert(Character::new(CharacterType::Warrior));
 }
